183e5b22d5a5859e2eeacfad56ac95c5482cb4a4
Tester86 <48809498+Tester86@users.noreply.github.com>
2019-09-12 16:41:49 +0200

    Create setup.bat

---
+++ libs/setup.bat
@@ -0,0 +1,10 @@
+echo "Starting download..."
+echo "Installing libs for music files"
+py -m pip install pywin32
+py -m pip install youtube-dl
+py -m pip install winsound
+echo "Installing libs for assistant"
+py -m pip install smtplib
+echo "Installing libs for manager"
+py -m pip install colorama
+py -m pip install shutil
